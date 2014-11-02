11597766bd4f4f6a1f810de1c0158ac200166a3c
Dayle Woolston <dayle.woolston@familysearch.org>
2014-11-02 13:08:37 -0700

    minor enum work

---
+++ enum/src/main/java/org/pdp/examples/java7/enums/EnumExample.java
@@ -0,0 +1,34 @@
+package org.pdp.examples.java7.enums;
+
+public class EnumExample {
+
+  public enum NorseGods {
+    ODIN('M'),
+    THOR('M'),
+    LOKI('M'),
+    FREYA('F'),
+    HEL('F'),
+    TYR('M'),
+    FENRIR('M');
+
+    private final char gender;
+
+    private NorseGods(char gender) {
+      this.gender = Character.toUpperCase(gender);
+    }
+
+    public boolean isMale() {
+      return 'M' == gender;
+    }
+
+    public boolean isFemale() {
+      return !isMale();
+    }
+  }
+
+  public static void main(String[] args) {
+
+    System.out.println(String.format("name: %s, isMale: %b", NorseGods.ODIN, NorseGods.ODIN.isMale()));
+    System.out.println(String.format("name: %s, isMale: %b", NorseGods.FREYA, NorseGods.FREYA.isMale()));
+  }
+}
