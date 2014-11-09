bd203153028760913d51cd39eb688bce10bd6f4d
Dayle Woolston <dayle.woolston@familysearch.org>
2014-11-09 08:44:37 -0700

    refactored logging example

--- logging/src/main/java/org/pdp/examples/java7/logging/App.java
+++ logging/src/main/java/org/pdp/examples/java7/logging/App.java
@@ -11,58 +11,73 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class App {
-    private final Logger slf4jLogger = LoggerFactory.getLogger(App.class);
-    private final String ipAddress = getIPAddress();
-    private final String hostName = getHostName();
 
+  private static final Logger SLF4J_LOGGER = LoggerFactory.getLogger(App.class);
 
-// got this from http://stackoverflow.com/questions/7348711/recommended-way-to-get-hostname-in-java
-    private String getHostName(){
-        String hostName;
-        try {
-            hostName = InetAddress.getLocalHost().getHostName();
-        } catch (UnknownHostException e){
-            hostName = null;
+  public static void main(String[] args) {
+
+    HostDescriptor descriptor = new HostDescriptor();
+    descriptor.describe();
+
+    SLF4J_LOGGER.info("timestamp=\"{}\" hostname=\"{}\" ip=\"{}\" message=\"{}\"", new Date(), descriptor.getHostName(), descriptor.getIpAddress(), "my info message");
+    SLF4J_LOGGER.debug("timestamp=\"{}\" hostname=\"{}\" ip=\"{}\" message=\"{}\"", new Date(), descriptor.getHostName(), descriptor.getIpAddress(), "my debug message");
+  }
+
+  private static final class HostDescriptor {
+
+    private String ipAddress = null;
+    private String hostName = null;
+
+    public String getIpAddress() {
+      return ipAddress;
+    }
+
+    public String getHostName() {
+      return hostName;
+    }
+
+    public void describe() {
+      ipAddress = discoverIpAddress();
+      hostName = discoverHostName();
+    }
+
+    // http://stackoverflow.com/questions/7348711/recommended-way-to-get-hostname-in-java
+    private String discoverHostName() {
+      String hostName;
+      try {
+        hostName = InetAddress.getLocalHost().getHostName();
+      }
+      catch (UnknownHostException e) {
+        hostName = null;
+      }
+      return hostName;
+    }
+
+    // http://stackoverflow.com/questions/8083479/java-getting-my-ip-address
+    private String discoverIpAddress() {
+      String ip = null;
+      try {
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        while (interfaces.hasMoreElements()) {
+          NetworkInterface iface = interfaces.nextElement();
+          // filters out 127.0.0.1 and inactive interfaces
+          if (iface.isLoopback() || !iface.isUp()) {
+            continue;
+          }
+
+          Enumeration<InetAddress> addresses = iface.getInetAddresses();
+          while (addresses.hasMoreElements()) {
+            InetAddress addr = addresses.nextElement();
+            ip = addr.getHostAddress();
+            System.out.println(iface.getDisplayName() + " " + ip);
+          }
         }
-        return hostName;
+      }
+      catch (SocketException e) {
+        ip = null;
+      }
+      return ip;
     }
+  }
 
-// got this code from http://stackoverflow.com/questions/8083479/java-getting-my-ip-address
-    private String getIPAddress(){
-        String ip=null;
-        try {
-            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-            while (interfaces.hasMoreElements()) {
-                NetworkInterface iface = interfaces.nextElement();
-                // filters out 127.0.0.1 and inactive interfaces
-                if (iface.isLoopback() || !iface.isUp())
-                    continue;
-
-                Enumeration<InetAddress> addresses = iface.getInetAddresses();
-                while(addresses.hasMoreElements()) {
-                    InetAddress addr = addresses.nextElement();
-                    ip = addr.getHostAddress();
-                    System.out.println(iface.getDisplayName() + " " + ip);
-                }
-            }
-        } catch (SocketException e) {
-            ip = null;
-        }
-        return ip;
-    }
-
-    private void logInfo(String message){
-        slf4jLogger.info("timestamp=\"{}\" hostname=\"{}\" ip=\"{}\" message=\"{}\"", new Date(), hostName, ipAddress, message);
-    }
-
-    private void logDebug(String message){
-        slf4jLogger.debug("timestamp=\"{}\" hostname=\"{}\" ip=\"{}\" message=\"{}\"", new Date(), hostName, ipAddress, message);
-    }
-
-    public static void main( String[] args ) {
-        App app = new App();
-
-        app.logInfo("Logging Info");
-        app.logDebug("Logging Debug");
-    }
 }
